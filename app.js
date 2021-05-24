4f71a95331ea8d43c357cf588ef0c8316cba3001
Alec <alecech1994@gmail.com>
2021-05-24 13:37:52 -0700

    Checks if user has won after making a move

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ for (let i = 1; i < 10; i++) {
   boardState[i] = '';
 }
 
-const updateBoard = (e) => {
-  boardState[e.target.innerText] = turn.player;
+const checkBoardClick = (e) => {
+  if (e.target.matches('.box')) {
+    updateGameState(e);
+  }
+}
+
+const updateBoard = (e, space) => {
+  boardState[space] = turn.player;
   e.target.innerText = turn.player;
   turn.numPlays++;
 }
@@ -20,33 +26,58 @@ const changePlayer = () => {
   turn.toggle();
 }
 
-const checkForWinner = () => {
-  if (turn.numPlays < 5) {
-    return;
+checkHorizontalWin = (lastPlay) => {
+  let matches = 0;
+  let rowPlayed = Math.floor((lastPlay - 1) / 3);
+  for (let key in boardState) {
+    if (Math.floor((key - 1) / 3) === rowPlayed) {
+      matches += boardState[key] === turn.player ? 1 : 0;
+    }
   }
-  // logic to check for victor
+  return matches === 3;
+}
 
-  // catch case where we have made it to end of game with tie
-  turn.numPlays === 9 ? console.log('tie game') : console.log('still playing');
-  // display tie
-  // reset game
+checkVerticalWin = (lastPlay) => {
+  let matches = 0;
+  let colPlayed = lastPlay % 3;
+  for (let key in boardState) {
+    if (key % 3 === colPlayed) {
+      matches += boardState[key] === turn.player ? 1 : 0;
+    }
+  }
+  return matches === 3;
+}
+
+checkDiagonalWin = (lastPlay) => {
+  return ((boardState[1] === boardState[5] && boardState[1] === boardState[9]) ||
+          (boardState[3] === boardState[5] && boardState[3] === boardState[7]));
+}
+
+const checkForWinner = (lastPlay) => {
+  if (turn.numPlays < 5) {
+    return false;
+  } else {
+    return checkHorizontalWin(lastPlay) || checkVerticalWin(lastPlay) || checkDiagonalWin(lastPlay);
+  }
+  console.log('tie game');
 }
 
 // Controller (Event Handlers)
 const updateGameState = (e) => {
   // need to find a better way to get value of box
-  if (boardState[e.target.innerText] === '') {
-    updateBoard(e);
+  let space = e.target.innerText
+  console.log(e.target.getAttribute('value'));
+  if (boardState[space] === '') {
+    updateBoard(e, space);
+    if(checkForWinner(space)){
+      document.removeEventListener('click', checkBoardClick);
+      console.log(`${turn.player} wins!`);
+    }
     changePlayer();
-    checkForWinner();
   } else {
     window.alert('That space is already occupied. Please choose another.');
   }
 }
 
 // View (Event Listeners)
-document.addEventListener('click', (e) => {
-  if (e.target.matches('.box')) {
-    updateGameState(e);
-  }
-});
+document.addEventListener('click', checkBoardClick);
