936a84cb17ee51f6a51e586c098feaa39c168170
Alec <alecech1994@gmail.com>
2021-05-24 12:27:45 -0700

    Improved separation of concerns

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 // Model
-const OPTIONS = ['X', 'O'];
-let playerTurn = 'X';
-let turn = 0;
+const turn = {
+  player: 'X',
+  numPlays: 0,
+  toggle: () => turn.player = turn.player === 'X' ? 'O' : 'X'
+};
+
+const boardState = {};
+for (let i = 1; i < 10; i++) {
+  boardState[i] = '';
+}
+
+const updateBoard = (e) => {
+  boardState[e.target.innerText] = turn.player;
+  e.target.innerText = turn.player;
+  turn.numPlays++;
+}
 
 const changePlayer = () => {
-  playerTurn = playerTurn === 'X' ? 'O' : 'X';
+  turn.toggle();
+}
+
+const checkForWinner = () => {
+  if (turn.numPlays < 5) {
+    return;
+  }
+  // logic to check for victor
+
+  // catch case where we have made it to end of game with tie
+  turn.numPlays === 9 ? console.log('tie game') : console.log('still playing');
+  // display tie
+  // reset game
 }
 
 // Controller (Event Handlers)
-const displaySymbol = (e) => {
-  if (!OPTIONS.includes(e.target.innerText)) {
-    e.target.innerText = playerTurn;
-    changePlayer(playerTurn);
+const updateGameState = (e) => {
+  // need to find a better way to get value of box
+  if (boardState[e.target.innerText] === '') {
+    updateBoard(e);
+    changePlayer();
+    checkForWinner();
+  } else {
+    window.alert('That space is already occupied. Please choose another.');
   }
 }
 
 // View (Event Listeners)
 document.addEventListener('click', (e) => {
   if (e.target.matches('.box')) {
-    displaySymbol(e);
+    updateGameState(e);
   }
 });
